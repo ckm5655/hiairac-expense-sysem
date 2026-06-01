a9a68074cabbc2cede024190a4311c1d6dab728b
ckm5655 <ckm5655@gmail.com>
2026-06-02 08:42:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 import json
 from datetime import datetime
-
+ 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
 
