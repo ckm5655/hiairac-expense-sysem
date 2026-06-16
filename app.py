2631954c1d04d5508612fdd1bd3a1cfa989d7ae1
ckm5655 <ckm5655@gmail.com>
2026-06-16 13:19:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ app.secret_key = 'your_secret_key_here'
 TEAM_BUDGETS = {
     "시운전팀": 1000000,
     "생산팀": 500000,
+    "판금생산팀" : 0,
     "생산설계팀": 0,
     "영업팀": 500000,
     "영업2팀": 0,
@@ -34,6 +35,7 @@ USER_CREDENTIALS = {
     "admin": {"password": "01234", "name": "관리자", "team": "관리자"},
     "시운전": {"password": "1234", "name": "시운전", "team": "시운전팀"},
     "생산": {"password": "1234", "name": "생산", "team": "생산팀"},
+    "판금": {"password": "1234", "name": "판금생산", "team": "판금생산팀"},
     "생산설계": {"password": "1234", "name": "생산설계", "team": "생산설계팀"},
     "영업": {"password": "1234", "name": "영업", "team": "영업팀"},
     "영업2": {"password": "1234", "name": "영업2", "team": "영업2팀"},
